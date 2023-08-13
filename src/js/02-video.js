b1d5e6570d43f8672dd0e2d889d9bdbcaf52ff03
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-08-13 21:22:35 +0300

    hw_8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = event => {
+  console.log(event);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+let feedbackDataToLs = {};
+const {
+  elements: { email, message },
+} = feedbackForm;
+
+restoreUserInputs();
+
+feedbackForm.addEventListener('input', throttle(saveFeedbackData, 500));
+feedbackForm.addEventListener('submit', onSubmitForm);
+
+function saveFeedbackData(e) {
+  feedbackDataToLs[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackDataToLs));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  //const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(feedbackDataToLs);
+  feedbackDataToLs = {};
+  localStorage.removeItem('feedback-form-state');
+}
+
+function restoreUserInputs() {
+  const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedFeedback) {
+    message.value = savedFeedback.message || '';
+    email.value = savedFeedback.email || '';
+    feedbackDataToLs[email.name] = savedFeedback.email;
+    feedbackDataToLs[message.name] = savedFeedback.message;
+  }
+}
